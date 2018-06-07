7e233b5df275a234293ef7d266a108cdf0bf2274
Ryan Bates <rbates214@gmail.com>
2018-06-07 15:43:22 -0500

    clean up

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -1,20 +1,26 @@
-﻿public enum Type { Fire, Water, Grass, Electric, Fairy, Dragon, Flying, Rock, Steel, Normal, Ground, Fighting }
+﻿using UnityEngine;
+
+public enum Type { Fire, Water, Grass, Electric, Fairy, Dragon, Flying, Rock, Steel, Normal, Ground, Fighting }
+public enum Moves { }
 public enum Gender { Male, Female }
 
-public interface ICreature
+[CreateAssetMenu]
+public class Creature : ScriptableObject
 {
-    float HP { get; set; }
-    float EXP { get; set; }
-    float Attack { get; set; }
-    float Speed { get; set; }
-    float Defence { get; set; }
+    public int LVL;
 
-    Type type { get; set; }
-    Gender gender { get; set; }
+    public float HP;
+    public float EXP;
+    public float Attack;
+    public float Speed;
+    public float Defence;
 
-    bool inEffective { get; set; }
-    bool notVeryEffective { get; set; }
-    bool superEffective { get; set; }
-    bool criticalHit { get; set; }
-    bool wild { get; set; }
+    public Type type;
+    public Gender gender;
+
+    public bool inEffective;
+    public bool notVeryEffective;
+    public bool superEffective;
+    public bool criticalHit;
+    public bool wild;
 }
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -2,37 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Monster : MonoBehaviour
+public class WildFight : MonoBehaviour
 {
-
+    private bool appear = false;
+    public float level;
+    public float time;
 
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
             DecideGender();
+
+        GetType();
     }
+
+    /// <summary>
+    /// have a transform start this and once both transfroms 
+    /// have touched for >= the time presented in the parameters
+    /// begin BeginBattle
+    /// </summary>
+    /// <param name="time"></param>
+    private void BeginBattle(float time)
+    {
+
+
+    }
+    
+
+    private void GetType()
+    {
+        var getType = new Type();
+
+        getType = (Type)Random.Range(0, 12);
+
+        Debug.Log("the type is:" + getType.ToString());
+    }
+
 
     void DecideGender()
     {
         var getGender = new Gender();
-        getGender = (Gender)Random.Range(0, 1);
+        getGender = (Gender)Random.Range(0, 2);
 
 
         if (getGender == Gender.Male)
         {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var boycube = GameObject.CreatePrimitive(PrimitiveType.Cube);
             var color = new Color(0, 0, 255);
-            cube.GetComponent<Renderer>().material.color = color;
-            Instantiate<GameObject>(cube, new Vector3(0, 1.5f, 0), gameObject.transform.rotation, gameObject.transform);
+            boycube.GetComponent<Renderer>().material.color = color;
+            Instantiate<GameObject>(boycube, new Vector3(0, 1.5f, 0), gameObject.transform.rotation, gameObject.transform);
+            Debug.Log("its a boy");
         }
 
         if (getGender == Gender.Female)
         {
-            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            var color = new Color(255, 192, 203);
-            cube.GetComponent<Renderer>().material.color = color;
-            Instantiate<GameObject>(cube, new Vector3(0, 1.5f, 0), gameObject.transform.rotation, gameObject.transform);
+            var girlcube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var color = new Color(255, 0, 255);
+            girlcube.GetComponent<Renderer>().material.color = color;
+            Instantiate<GameObject>(girlcube, new Vector3(0, 1.5f, 0), gameObject.transform.rotation, gameObject.transform);
+            Debug.Log("its a girl");
         }
     }
 
